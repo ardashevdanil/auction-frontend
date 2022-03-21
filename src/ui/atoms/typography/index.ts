158476c4da09ee442725d360547863853e7b4971
Demyan Dubovik <demyan.dubovik@soulplatform.com>
2022-03-21 19:10:44 +0300

    Typography lint

--- src/ui/atoms/typography/index.ts
+++ src/ui/atoms/typography/index.ts
@@ -1,16 +1,16 @@
-import { H1 } from "./h1"
-import { H2 } from "./h2"
-import { H3 } from "./h3"
-import { H4 } from "./h4"
-import { H5 } from "./h5"
-import { H6 } from "./h6"
-import { Subtitle1 } from "./subtitle1"
-import { Subtitle2 } from "./subtitle2"
-import { Body1 } from "./body1"
-import { Body2 } from "./body2"
-import { ButtonText as Button } from "./button"
-import { Caption } from "./caption"
-import { Overline } from "./overline"
+import { H1 } from "./h1";
+import { H2 } from "./h2";
+import { H3 } from "./h3";
+import { H4 } from "./h4";
+import { H5 } from "./h5";
+import { H6 } from "./h6";
+import { Subtitle1 } from "./subtitle1";
+import { Subtitle2 } from "./subtitle2";
+import { Body1 } from "./body1";
+import { Body2 } from "./body2";
+import { ButtonText as Button } from "./button";
+import { Caption } from "./caption";
+import { Overline } from "./overline";
 
 export const Typography = {
   H1,
@@ -26,4 +26,4 @@ export const Typography = {
   Button,
   Caption,
   Overline,
-}
+};
